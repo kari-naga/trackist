ea75ebeca0a65aad2391b9a1ceef1fa85a9e822a
Kari Naga <4119937+kari-naga@users.noreply.github.com>
2022-10-25 23:14:06 -0400

    Setup project

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default async function Home() {
+  return (
+    <div className="h-screen p-4 flex flex-col justify-center items-center gap-4">
+      <h1 className="text-3xl font-medium tracking-wide">Trackist</h1>
+      <IconBiBadgeHd />
+    </div>
+  )
+}
--- next.config.js
+++ next.config.js
@@ -2,6 +2,52 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  webpack(config) {
+    config.plugins.push(
+      require('unplugin-icons/webpack')({
+        compiler: 'jsx',
+        jsx: 'react',
+        autoInstall: true,
+      }),
+      require('unplugin-auto-import/webpack')({
+        include: [
+          /\.[tj]sx?$/,
+          /\.md$/,
+        ],
+        imports: [
+          'react',
+          {
+            'next/link': [['default', 'Link']],
+            'next/image': [['default', 'Image']],
+            'next/script': [['default', 'Script']],
+            'next/head': [['default', 'Head']],
+            'next/headers': ['cookies', 'headers'],
+            'next/dist/client/components/not-found': ['notFound'],
+            'next/navigation': ['useRouter', 'useSearchParams', 'useSelectedLayoutSegment', 'useSelectedLayoutSegments', 'usePathname'],
+          }
+        ],
+        dirs: [
+          './app/**',
+          './pages/**',
+        ],
+        resolvers: [
+          require('unplugin-icons/resolver')({
+            prefix: 'Icon',
+            extension: 'jsx',
+          }),
+        ],
+        dts: true,
+        eslintrc: {
+          enabled: true,
+        },
+      }),
+    )
+
+    return config
+  },
+  experimental: {
+    appDir: true,
+  },
 }
 
 module.exports = nextConfig
