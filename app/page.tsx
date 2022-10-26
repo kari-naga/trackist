4068fe5c1103d7653813aab1afd67971f3a560a7
Kari Naga <4119937+kari-naga@users.noreply.github.com>
2022-10-25 23:42:15 -0400

    Remove auto-import

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
+import IconHD from '~icons/bi/badge-hd'
+
 export default async function Home() {
   return (
     <div className="h-screen p-4 flex flex-col justify-center items-center gap-4">
       <h1 className="text-3xl font-medium tracking-wide">Trackist</h1>
-      <IconBiBadgeHd />
+      <IconHD />
     </div>
   )
 }
